361aab07e704583a610e9907fbf258d589391124
sriramgupta-3112 <sriram.gupta@gmail.com>
2026-02-12 10:52:25 -0600

    Update trainglescreener.py

--- trainglescreener.py
+++ trainglescreener.py
@@ -15,7 +15,7 @@ from datetime import datetime, timedelta, timezone
 # ==========================================
 
 # 🔐 SECURITY SETTINGS
-APP_PASSWORD = "trading-god-mode" 
+APP_PASSWORD = "JaiBabaKi" 
 
 # 📱 TELEGRAM SETTINGS
 TELEGRAM_BOT_TOKEN = "YOUR_BOT_TOKEN_HERE" 
@@ -431,3 +431,4 @@ else:
                             
                     except Exception as e:
                         st.error(f"Data Error: {e}")
+
